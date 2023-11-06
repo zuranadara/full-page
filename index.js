cb6081fd4c4b406b3aeaabe8542343f6285c89f0
zura nadareishvili <zuranadareishvili07@gmail.com>
2023-11-07 00:16:44 +0400

    ganaxlebuli

---
+++ index.js
@@ -0,0 +1,14 @@
+const newPrise = 50
+const oldPrice = 80
+const sale = ((newPrise-oldPrice)/oldPrice*100)
+console.log('fasdakleba aris = ', sale);
+
+
+const car = {
+    maxSpeed: 200,
+    color: 'silver',
+    doorAmount: 4
+}
+
+console.log('maqsimaluri sichkare aris:', car.maxSpeed, 'feri aris:', car.color, 'karebebis raodenoba aris:', car.doorAmount)
+
